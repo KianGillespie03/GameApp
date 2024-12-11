815f1b6adf921c4b98c721aa4e8af0c7a2b4da7a
kian Gillespie <s00235539@atu.ie>
2024-12-11 11:35:54 +0000

    Opening screen added and play button tskes to game page added

--- app/src/main/java/com/example/gameapp/MainActivity.java
+++ app/src/main/java/com/example/gameapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.gameapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Set up the Play button click listener
+        Button playButton = findViewById(R.id.PlayButtton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
